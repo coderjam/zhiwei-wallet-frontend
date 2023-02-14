5abc9f2de05401ff1c562ed2eb690aa4113e90fb
jam <jamis404@163.com>
2023-02-14 11:42:32 +0800

    一个简单的计数写法

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { defineComponent } from "vue";
+import {defineComponent, ref} from "vue";
 
 export const App = defineComponent({
-    setup(){
-        return ()=> (<div>hi app.tsx</div>)
+    setup() {
+        const refCount = ref(0)
+        const onClick = () => {
+            refCount.value += 1
+        }
+        return () => <>
+            <div>
+                {refCount.value}
+            </div>
+            <div>
+                <button onClick={onClick}>+1</button>
+            </div>
+        </>
     }
 })
