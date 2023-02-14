e28aa8f92e1440118c57304b90035ec30798c571
jam <jamis404@163.com>
2023-02-14 11:17:29 +0800

    引入@vitejs/plugin-vue-jsx并配置vite.config.ts支持TSX。更改vite版本号

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/zhiwei-wallet-frontend/dist/',
-  plugins: [vue()]
+    base: '/zhiwei-wallet-frontend/dist/',
+    plugins: [
+        vue(),
+        vueJsx({
+            // options are passed on to @vue/babel-plugin-jsx
+        }),
+    ]
 })
