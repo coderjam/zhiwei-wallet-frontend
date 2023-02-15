b4b436f87c6f9ed8b7cbc83487650c45cd4ac57f
jam <jamis404@163.com>
2023-02-15 13:26:26 +0800

    改为函数式组件

--- src/components/Welcome/WelcomeLayout.tsx
+++ src/components/Welcome/WelcomeLayout.tsx
@@ -1,18 +1,17 @@
-import { defineComponent } from 'vue';
+import {FunctionalComponent} from 'vue';
 import s from './WelcomeLayout.module.scss';
-export const WelcomeLayout = defineComponent({
-    setup: (props, context) => {
-        const { slots } = context
-        return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    { slots.icon?.() }
-                    { slots.title?.() }
-                </div>
-                <div class={s.actions}>
-                    { slots.buttons?.() }
-                </div>
+
+export const WelcomeLayout: FunctionalComponent = (props, {slots}) => {
+    const {icon, title, buttons} = slots
+    return (
+        <div class={s.wrapper}>
+            <div class={s.card}>
+                {icon?.()}
+                {title?.()}
             </div>
-        )
-    }
-})
+            <div class={s.actions}>
+                {buttons?.()}
+            </div>
+        </div>
+    )
+}
