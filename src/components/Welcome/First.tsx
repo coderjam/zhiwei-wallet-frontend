5efbec0e9004d126197276f1b19636258e2e4277
jam <jamis404@163.com>
2023-02-15 13:19:08 +0800

    defineComponent改为FunctionalComponent（函数式组件）

--- src/components/Welcome/First.tsx
+++ src/components/Welcome/First.tsx
@@ -1,24 +1,20 @@
-import {defineComponent} from 'vue';
+import {FunctionalComponent} from 'vue';
 import s from './WelcomeLayout.module.scss';
 import pig from '../../assets/icons/pig.svg';
 import {WelcomeLayout} from "./WelcomeLayout";
 import {RouterLink} from 'vue-router';
 
-export const First = defineComponent({
-    setup: (props, context) => {
-        return () => (
-            <WelcomeLayout>{
-                {
-                    icon: () => <img src={pig}/>,
-                    title: () => <h2>会挣钱<br/>还要会省钱</h2>,
-                    buttons: () => <>
-                        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                        <RouterLink to="/welcome/2">下一页</RouterLink>
-                        <RouterLink to="/start">跳过</RouterLink>
-                    </>
-                }
-            }
-            </WelcomeLayout>
-        )
+export const First: FunctionalComponent = () => {
+    return <WelcomeLayout>{
+        {
+            icon: () => <img src={pig}/>,
+            title: () => <h2>会挣钱<br/>还要会省钱</h2>,
+            buttons: () => <>
+                <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                <RouterLink to="/welcome/2">下一页</RouterLink>
+                <RouterLink to="/start">跳过</RouterLink>
+            </>
+        }
     }
-})
+    </WelcomeLayout>
+}
